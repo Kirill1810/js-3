3e0c8b82efea4c6f60dbe484d07e7effbc9c4fbd
Kirill1810 <129986639+Kirill1810@users.noreply.github.com>
2024-09-23 18:00:13 +0300

    прапарап

---
+++ js.js
@@ -0,0 +1,26 @@
+// 1
+const result= "5+5+5";
+
+console.log (result)
+
+//2
+
+const email = "ivanovkirill1810@gmail.com"
+
+const num= email.includes("@");
+
+console.log (num )
+console.log (email.length )
+
+//3
+
+const name1 = "my name "
+const name2 ="viktor"
+console.log (name1  +  name2)
+
+//4
+
+const userName = "Олександре";
+const payment = 300;
+
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
